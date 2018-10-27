661c76834c417427053a182d816a9463937b188d
Barbany <oriol.barbany@alu-etsetb.upc.edu>
2018-10-27 17:18:37 +0200

    Don't include plots to github

--- src/ml_methods/cross_validation.py
+++ src/ml_methods/cross_validation.py
@@ -6,7 +6,7 @@ from utils.helpers import build_poly, build_k_indices
 
 from preproc.data_clean import load_csv_data_no_na
 from utils.costs import compute_loss, accuracy
-from ml_methods.implementations import ridge_regression, logistic_regression, reg_logistic_regression
+from ml_methods.implementations import *
 from utils.helpers import predict_labels, predict_labels_logistic
 from utils.helpers import standardize, standardize_by_feat
 import matplotlib.pyplot as plt
@@ -14,6 +14,16 @@ import sys
 
 
 def get_data_cv(y, x, k_indices, k, degree):
+    """
+    Get train and test data from number of fold and randomized indexes. Returns data after applying polynomial of
+    a certain degree
+    :param y: Labels
+    :param x: Features
+    :param k_indices: Random indices obtained with build_k_indices
+    :param k: Number of fold that will be used as train
+    :param degree: Degree of polynomial to use
+    :return: tx_train, y_train, tx_test, y_test
+    """
     x_test = x[k_indices[k]]
     x_train = np.delete(x, k_indices[k], axis=0)
     y_test = y[k_indices[k]]
@@ -22,13 +32,16 @@ def get_data_cv(y, x, k_indices, k, degree):
     tx_train = build_poly(x_train, degree)
     tx_test = build_poly(x_test, degree)
 
+    # Standardize test and train with train values for each feature
+    # Note that first column is not included since it's the bias term
     tx_train[:, 1:], mean_x, std_x = standardize_by_feat(tx_train[:, 1:])
     tx_test[:, 1:] = (tx_test[:, 1:] - mean_x) / std_x
 
     return tx_train, y_train, tx_test, y_test
 
 
-def cross_validation(y, x, k_fold, lambdas, degrees, max_iters, gamma, seed=123, verbose=False, jet=0):
+def cross_validation(y, x, k_fold, lambdas, degrees, max_iters, gamma, loss_function, batch_size=None,
+                     seed=123, verbose=False, jet=0, mass=False):
     """
     K-fold cross validation
     :param y: labels
@@ -38,8 +51,12 @@ def cross_validation(y, x, k_fold, lambdas, degrees, max_iters, gamma, seed=123,
     :param degrees: range of degrees to consider
     :param max_iters: Maximum number of iterations
     :param gamma: Learning rate
+    :param loss_function: Loss function
+    :param batch_size: Data points considered in each gradient (Default: None, i.e. GD - not SGD)
     :param seed: random seed
     :param verbose: Print information
+    :param jet: Number of jet if we are using it
+    :param mass: Boolean indicating if we are splitting with mass
     :return: minimum loss (min_loss) and optimal value for lambda (best_lambda) and degree (best_degree)
     """
 
@@ -62,7 +79,12 @@ def cross_validation(y, x, k_fold, lambdas, degrees, max_iters, gamma, seed=123,
             for k in range(k_fold):
                 tx_tr, y_tr, tx_te, y_te = get_data_cv(y, x, k_indices, k, degree)
                 initial_w = np.zeros(tx_tr.shape[1])
-                w, loss = reg_logistic_regression(y_tr, tx_tr, lambda_, initial_w, max_iters, gamma)
+                if batch_size is None:
+                    w, loss = least_square_gd(y_tr, tx_tr, initial_w, max_iters,
+                                              gamma, loss_function=loss_function, lambda_=lambda_)
+                else:
+                    w, loss = least_square_sgd(y_tr, tx_tr, initial_w, batch_size, max_iters,
+                                               gamma, loss_function=loss_function, lambda_=lambda_)
                 y_pred_tr = predict_labels_logistic(w, tx_tr)
                 loss = accuracy(y_tr, y_pred_tr)
                 loss_tr.append(loss)
@@ -82,10 +104,10 @@ def cross_validation(y, x, k_fold, lambdas, degrees, max_iters, gamma, seed=123,
         plt.plot(lambdas, loss_lambdas_te, 'b-', label='Test')
         plt.plot(lambdas, loss_lambdas_tr, 'r-', label='Train')
         plt.legend(loc='upper left')
-        plt.title('Loss evolution for degree ' + str(degree) + ' in jet ' + str(jet))
+        plt.title('Loss evolution for degree ' + str(degree) + ' in jet ' + str(jet) + ' with mass'*mass)
         plt.ylabel('Loss')
         plt.xlabel('Lambda')
-        plt.savefig('../results/plots/plot_degree_' + str(degree) + '_jet_' + str(jet) + '.png')
+        plt.savefig('../results/plots/plot_degree_' + str(degree) + '_jet_' + str(jet) + ' w_mass'*mass + '.png')
         plt.clf()
 
         if verbose:
